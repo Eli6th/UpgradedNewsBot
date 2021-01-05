5bd7e86891ab8fdd8c5c795eddf5b45b6b1a8f6f
Eli6th <57406743+Eli6th@users.noreply.github.com>
2021-01-04 21:02:30 -0500

    Update importInfo.py

--- importInfo.py
+++ importInfo.py
@@ -3,8 +3,14 @@ newyorktimesArticleAPIKey =
 gmail_from_address = 
 gmail_from_address_password = 
 
-weather_sources = 
-place = 
-news_sources = 
-news_keywords =     
-email_addresses = 
+weather_sources = ['OpenWeather']
+place = [['', ''], ['', ''], ['', ''], ['', ''], ['', '']] #enter coordinates from where each user lives
+news_sources = ['GoogleNews', 'NewYorkTimes', ''] #delete as desired
+news_keywords = [
+  ['Python Projects', 'Python Automation Ideas'], #will send all of these subjects to the 1st in the array: 'what@gmai.com'
+  ['AI', 'Machine Learning', 'Coding Projects'], #will send to the 2nd in the array: 'ever@gmail.com'
+  ['Tesla', 'Elon Musk', 'Space X'], #will send to the 3rd in the array: 'your@gmail.com'
+  ['Finance', 'Soccer', 'Manchester City'], #will send to the 4th in the array: 'gmail@gmail.com'
+  ['NBA', 'NHL'] #will send to the 5th in the array: 'is@gmail.com'
+]    
+email_addresses = ['what@gmai.com', 'ever@gmail.com', 'your@gmail.com', 'gmail@gmail.com', 'is@gmail.com']
