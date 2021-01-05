12f264433dfbf20ee7ab4ee691fa51e3df424a61
Eli6th <57406743+Eli6th@users.noreply.github.com>
2021-01-04 20:47:34 -0500

    Add files via upload

---
+++ importInfo.py
@@ -0,0 +1,10 @@
+openweatherAPIKEY = 
+newyorktimesArticleAPIKey = 
+gmail_from_address = 
+gmail_from_address_password = 
+
+weather_sources = 
+place = 
+news_sources = 
+news_keywords =     
+email_addresses = 
